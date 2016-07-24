6c0634d6bbb56e6752783ad4dc235d4153996c1f
Sergey Tselovalnikov <sergeicelov@gmail.com>
2016-07-24 21:26:08 +0300

    release Franky-0.1.0-SNAPSHOT.zip

--- release.sh
+++ release.sh
@@ -6,7 +6,9 @@ cd ../franky
 ./gradlew buildPlugin
 cd -
 cp ../franky/franky-intellij/build/distributions/* .
+FRANKY_RELEASE_NAME=`ls -1 | grep zip`
+sed -e "s/\$NAME/$FRANKY_RELEASE_NAME/" updatePlugins.xml.dist > updatePlugins.xml
 git add .
-git commit -m"release" -a
+git commit -m"release $FRANKY_RELEASE_NAME" -a
 git push
 
